005de0164d36dff63603253b4005292fdbfa11ae
Dany Pratama <it.support@mandirialkesindo.com>
2023-07-11 17:34:05 +0700

    update 110723

--- cek-produk-inv-nonppn.php
+++ cek-produk-inv-nonppn.php
@@ -196,6 +196,15 @@ include "akses.php";
                                 </div>
                                 <div class="row">
                                     <div class="col-5">
+                                        <p style="float: left;">Pelanggan Inv</p>
+                                        <p style="float: right;">:</p>
+                                    </div>
+                                    <div class="col-7">
+                                        <?php echo $data['cs_inv'] ?>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-5">
                                         <p style="float: left;">Alamat</p>
                                         <p style="float: right;">:</p>
                                     </div>
@@ -234,8 +243,29 @@ include "akses.php";
 
                             </div>
                         </div>
+                        <div class="text-center mt-3 mb-3">
+                            <button class="btn btn-info btn-md" data-bs-toggle="modal" data-bs-target="#editPelanggan"><i class="bi bi-pencil"></i> Edit Pelanggan Invoice</button>
+                        </div>
+                        <!-- Modal -->
+                        <div class="modal fade" id="editPelanggan" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Pelanggan Invoice</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                   <form action="proses/">
+                                        <input type="text" class="form-control" value=" <?php echo $data['cs_inv'] ?>">
+                                   </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
                 </div>
                 <!-- Tampil data -->
                 <div class="card shadow">
@@ -791,14 +821,14 @@ include "akses.php";
                             <input type="text" class="form-control" name="resi" id="resi" style="display: none;">
                         </div>
                         <div class="mb-3">
-                            <label id="labelDate" style="display: none;">Tanggal</label>
-                            <input type="text" style="background-color:white; display: none;" class="bg-white form-control" name="tgl" id="date" required>
+                            <label id="labelDate">Tanggal</label>
+                            <input type="text" style="background-color:white;" class="bg-white form-control" name="tgl" id="date" required>
                         </div>
                         <div class="mb-3">
                             <label id="labelBukti1" style="display: none;">Bukti Terima 1</label>
                             <input type="file" name="fileku1" id="fileku1" accept="image/*" onchange="compressAndPreviewImage(event)" style="display: none;">
                         </div>
-                        <div class="mb-3" id="imagePreview" style="display: none;"></div>
+                        <div class="mb-3 preview-image" id="imagePreview" style="display: none;"></div>
 
                         <div class="mb-3">
                             <label id="labelBukti2" style="display: none;">Bukti Terima 2</label>
@@ -811,7 +841,7 @@ include "akses.php";
                         </div>
                         <div class="mb-3" id="imagePreview3" style="display: none;"></div>
                         <div class="modal-footer">
-                            <button type="submit" class="btn btn-primary" name="ubah-dikirim" id="dikirim"><i class="bi bi-arrow-left-right"></i> Ubah Status</button>
+                            <button type="submit" class="btn btn-primary" name="ubah-dikirim" id="dikirim" disabled><i class="bi bi-arrow-left-right"></i> Ubah Status</button>
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="cancelDikirim"><i class="bi bi-x-circle"> Cancel</i></button>
                         </div>
                     </form>
@@ -864,8 +894,6 @@ include "akses.php";
         const labelResi = document.getElementById('labelResi')
         const resiSelect = document.getElementById('resi');
         const dikirim = document.getElementById('dikirim');
-        const labelDate = document.getElementById('labelDate');
-        const inputDate = document.getElementById('date');
         const labelBukti1 = document.getElementById('labelBukti1');
         const labelBukti2 = document.getElementById('labelBukti2');
         const labelBukti3 = document.getElementById('labelBukti3');
@@ -876,9 +904,7 @@ include "akses.php";
         const imagePreview2 = document.getElementById('imagePreview2');
         const imagePreview3 = document.getElementById('imagePreview3');
 
-
         let isModalShown = false;
-
 
         jenisPengirimanSelect.addEventListener('change', function() {
             if (this.value === 'Driver') {
@@ -892,8 +918,6 @@ include "akses.php";
                 ekspedisiSelect.removeAttribute('required');
                 resiSelect.value = ''; // Mengatur ulang nilai menjadi kosong
                 resiSelect.removeAttribute('required');
-                labelDate.style.display = 'block'; // Menampilkan form input
-                inputDate.style.display = 'block'; // Menampilkan form input
                 labelResi.style.display = 'none'; // Menyembunyikan form input
                 labelBukti1.style.display = 'none'; // Menyembunyikan form input
                 labelBukti2.style.display = 'none'; // Menyembunyikan form input
@@ -939,9 +963,7 @@ include "akses.php";
                 pengirimSelect.style.display = 'none'; // Menyembunyikan form input
                 labelEkspedisi.style.display = 'none'; // Menyembunyikan form input
                 ekspedisiSelect.style.display = 'none'; // Menyembunyikan form input
-                resiSelect.style.display = 'none'; // Menyembunyikan form input
-                labelDate.style.display = 'none'; // Menyembunyikan form input
-                inputDate.style.display = 'none'; // Menyembunyikan form input
+                labelResi.style.display = 'none'; // Menyembunyikan form input
                 file1.style.display = 'none'; // Menyembunyikan form input
                 file2.style.display = 'none'; // Menyembunyikan form input
                 file3.style.display = 'none'; // Menyembunyikan form input
@@ -961,6 +983,7 @@ include "akses.php";
                 file1.style.display = 'none'; // Menyembunyikan form input
                 file2.style.display = 'none'; // Menyembunyikan form input
                 file3.style.display = 'none'; // Menyembunyikan form input
+                dikirim.disabled = true;
             });
 
             // Refresh halaman modal
@@ -993,8 +1016,6 @@ include "akses.php";
                 pengirimSelect.style.display = 'none'; // Menyembunyikan form input
                 labelResi.style.display = 'none'; // Menyembunyikan form input
                 resiSelect.style.display = 'none'; // Menyembunyikan form input
-                labelDate.style.display = 'none'; // Menyembunyikan form input
-                inputDate.style.display = 'none'; // Menyembunyikan form input
                 labelBukti1.style.display = 'none'; // Menyembunyikan form input
                 labelBukti2.style.display = 'none'; // Menyembunyikan form input
                 labelBukti3.style.display = 'none'; // Menyembunyikan form input
