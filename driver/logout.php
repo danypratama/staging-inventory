4d124dc62584b55d00c1efb7d91505f83e682a6a
Dany Pratama <it.support@mandirialkesindo.com>
2024-01-13 14:32:46 +0700

    130124

---
+++ driver/logout.php
@@ -0,0 +1,22 @@
+<?php
+// Koneksi
+include "koneksi.php";
+session_start();
+
+$ip = $_SESSION['ip'];
+
+$id_history = $_SESSION['encoded_id'];
+
+// Simpan Waktu pada database
+$id_user = $_SESSION['tiket_id'];
+$offline = 'Offline';
+$timezone = time() + (60 * 60 * 7);
+$today = gmdate('Y/m/d G:i:s', $timezone);
+unset($_SESSION['username']);
+unset($_SESSION['login']);
+// Simpan History
+$decode = base64_decode($id_history);
+$history = mysqli_query($connect, "UPDATE user_history SET logout_time = '$today', status_perangkat = '$offline' WHERE id_history = '$decode'");
+
+session_destroy();
+header("Location:../login.php");
